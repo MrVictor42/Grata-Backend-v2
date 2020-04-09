17325582260ef29dd95759cfc49ddc0bd9215a54
MrVictor42 <guin-hu@hotmail.com>
2020-04-09 15:28:20 -0300

    upload urls_user

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from users.api.views import UserViewSet, UserDetail, UserUpdate, UserDelete
 
 urlpatterns = [
     path('', UserViewSet.as_view({'get': 'list'})),
-    path('show_user/<int:pk>/', UserDetail.as_view()),
-    path('update_user/<int:pk>/', UserUpdate.as_view()),
-    path('delete_user/<int:pk>/', UserDelete.as_view())
+    path('user_detail/<int:pk>/', UserDetail.as_view()),
+    path('user_edit/<int:pk>/', UserUpdate.as_view()),
+    path('user_delete/<int:pk>/', UserDelete.as_view())
 ]
